a695b968c7b49ff40377a37c9ffa9c36385f57a1
eskovdmt <DmtEskov@gmail.com>
2023-11-23 15:25:15 +0300

    feat: add hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,8 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
-  config.hosts << ENV['DEVELOPMENT_HOSTS']
+  # config.hosts << ENV['DEVELOPMENT_HOSTS']
+  config.hosts << "rails-project-64-production-7ec8.up.railway.app"
 
   # Do not eager load code on boot.
   config.eager_load = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
-  config.hosts << ENV['DEVELOPMENT_HOSTS']
+  config.hosts << "rails-project-64-production-7ec8.up.railway.app"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
