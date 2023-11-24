a0cc7fea045e1236d351f75ae4f852d6869e3db5
eskovdmt <DmtEskov@gmail.com>
2023-11-24 21:52:37 +0300

    fix: change require test_helper

--- test/controllers/home_page_controller_test.rb
+++ test/controllers/home_page_controller_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require_relative '../test_helper'
 
 class HomePageControllerTest < ActionDispatch::IntegrationTest
   test 'should get home_page' do
