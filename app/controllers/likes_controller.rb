53c6ec7bf4fb65e57976c4c26027ebbd1f229ddd
eskovdmt <DmtEskov@gmail.com>
2023-12-17 17:19:25 +0300

    fix: fix reviewer remarks

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,6 +3,7 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: %i[create destroy]
+  before_action :authorize_like_owner!, only: %i[destroy]
 
   def create
     @like = @post.likes.new(user: current_user)
@@ -12,8 +13,6 @@ class LikesController < ApplicationController
 
   def destroy
     @like = PostLike.find(params[:id])
-    redirect_to root_path and return if @like.user != current_user
-
     @like.destroy
     redirect_to @post
   end
@@ -22,5 +21,12 @@ class LikesController < ApplicationController
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def authorize_like_owner!
+    @like = PostLike.find(params[:id])
+    return unless @like.user != current_user
+
+    redirect_to root_path
   end
 end
