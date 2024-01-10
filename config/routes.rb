ab166eacbe123dd41bd5b3db6a7d49dd0a73595f
eskovdmt <DmtEskov@gmail.com>
2024-01-10 11:15:28 +0300

    fix: add root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home_page#show'
   devise_for :users, controllers: { registrations: 'registrations',
                                     sessions: 'sessions',
                                     passwords: 'passwords' }
