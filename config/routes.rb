429c7d3c1c5893564e53b16666393d62f6ab7951
eskovdmt <DmtEskov@gmail.com>
2024-01-10 11:08:30 +0300

    fix: delete x symbol

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  x
   devise_for :users, controllers: { registrations: 'registrations',
                                     sessions: 'sessions',
                                     passwords: 'passwords' }
