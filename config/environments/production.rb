3f9b564bdc17266b663712d61dc0fad041a36ea7
Dmitry <DmtEskov@gmail.com>
2024-05-20 12:02:30 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.hosts << 'https://collectiveblog-production.up.railway.app/'
+  config.hosts << 'collectiveblog-production.up.railway.app'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
